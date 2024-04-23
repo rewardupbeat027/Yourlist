4e6c0f11ce5c43a1db8cda1b947ddc5e0db2b62c
D1M1TR1Y <dimaharcenko513@gmail.com>
2024-04-23 18:43:57 +0600

    База

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Purchase
+from .models import Purchase, Registration, ProductImage
 
 # Register your models here.
 admin.site.register(Purchase)
+admin.site.register(Registration)
+admin.site.register(ProductImage)
--- main/models.py
+++ main/models.py
@@ -1,15 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to='images/')
+
 
 class Purchase(models.Model):
     title = models.CharField('Name:', max_length=50)
     category = models.CharField('Category', max_length=50)
     text = models.CharField('Comment to purchase:', max_length=200)
     date = models.DateTimeField('Deadline time/date(example:2024-12-31):')
-    image = models.ImageField(blank=True, null=True)
+    image = models.ForeignKey(ProductImage, on_delete=models.CASCADE, related_name='images')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='registration_purchase')
 
     def __str__(self):
         return self.title
 
+
+class Registration(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='registration')
